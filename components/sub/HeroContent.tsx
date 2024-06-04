bd2f4c72e7c491129a08b363af4e7c85a4f7e98a
AlineTardoneHagens <aline.tardone@hagens.com.br>
2024-06-03 22:09:29 -0300

    ✨ add: Learn more button

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -2,8 +2,9 @@
 
 import React from 'react'
 import { motion } from 'framer-motion'
-import { slideInFromLeft, slideInFromTop } from '@/utils/motion'
+import { slideInFromLeft, slideInFromRight, slideInFromTop } from '@/utils/motion'
 import { SparklesIcon } from '@heroicons/react/24/solid'
+import Image from "next/image";
 
 const HeroContent = () => {
   return (
@@ -13,16 +14,16 @@ const HeroContent = () => {
       className='flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]'
     >
       <div className='h-full w-full flex flex-col gap-5 justify-center m-auto text-start'>
-        <motion.div
+        {/* <motion.div
           variants={slideInFromTop}
-          className='Welcome-box py-[8px] px-[4px] border border-[#7042f88b] opacity-[0,9]'>
+          className='Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0,9]'>
           <SparklesIcon className='text-[#b49bff] mr-[10px] h-5 w-5' />
-          <h1 className='Welcome-text text-[13px]'>Fullstack Developer Portfolio</h1>
-        </motion.div>
+          <h1 className='Welcome-text text-[13px]'>Fullstack Developer Portfolio </h1>
+        </motion.div> */}
 
         <motion.div
           variants={slideInFromLeft(0.5)}
-          className='flex flex-col gap-6 mt-6 text-6xl text-bold text-white max-w-[600px] w-auto h-auto'
+          className='flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto'
         >
           Aline Tardone
           <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500'>
@@ -35,8 +36,24 @@ const HeroContent = () => {
           className='text-lg text-gray-400 my-5 max-w-[600px]'>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tellus in hac habitasse platea dictumst vestibulum rhoncus est pellentesque
         </motion.p>
-      </div>
 
+        <motion.a
+          variants={slideInFromLeft(1)}
+          className='py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]'>
+          Saiba mais!
+        </motion.a>
+      </div>
+      <motion.div
+        variants={slideInFromRight(0.8)}
+        className="w-full h-full flex justify-center items-center"
+      >
+        <Image
+          src="/mainIconsdark.svg"
+          alt="work icons"
+          height={650}
+          width={650}
+        />
+      </motion.div>
     </motion.div>
   )
 }
